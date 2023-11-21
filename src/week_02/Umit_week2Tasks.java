1b0880edc69ffd807baaaee3e906f6ef965ac3d4
umittsr <umittsr@gmail.com>
2023-11-21 18:20:52 +0100

    Ümit's solutions for the tasks week 2

--- src/week_02/Umit_week2Tasks.java
+++ src/week_02/Umit_week2Tasks.java
@@ -1,5 +1,7 @@
 package week_02;
 
+import java.util.Scanner;
+
 public class Umit_week2Tasks {
 
     public static void main(String[] args) {
@@ -13,12 +15,71 @@ public class Umit_week2Tasks {
         b = a - b; // b = 30-10 = 20
         a = a - b; // a = 30-20 = 10
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a = " + a);  // 10
+        System.out.println("b = " + b);  // 20
 
 
+        //2- numbers divisible by 3, 5, 15
+
+        String divisibleBy15 = "";   //divisible by 3,5,15
+        String divisibleBy3 = "";    //divisible by 3 but NOT 15
+        String divisibleBy5 = "";    // divisible by 5 but NOT 15
+
+
+        for (int i = 1; i <=100 ; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0){
+               divisibleBy15 +=" "+ i;
+            }
+            if (i % 3 == 0 && i % 15 != 0){
+                divisibleBy3 +=" "+ i;
+            }
+            if (i % 5 == 0 && i % 15 != 0) {
+                divisibleBy5 += " "+ i;
+            }
+
+
+        }
+        System.out.println("divisibleBy15 = " + divisibleBy15); //divisibleBy15 =  15 30 45 60 75 90
+        System.out.println("divisibleBy3 = " + divisibleBy3);//divisibleBy3 =  3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
+        System.out.println("divisibleBy5 = " + divisibleBy5);//divisibleBy5 =  5 10 20 25 35 40 50 55 65 70 80 85 95 100
+
+
+
+        //3-Print consecutive numbers
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a positive number");
+
+        int numb = input.nextInt();
+
+        input.close();
+
+        String result = "";
+
+        for (int i = 1; i <= numb ; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                result += "CodilityTestCoders ";
+            }else if (i % 2 == 0 && i % 3 == 0) {
+                result += "CodilityTest ";
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                result += "CodilityCoders ";
+            }else if (i % 3 == 0 && i % 5 == 0) {
+                    result += "TestCoders ";
+            }else if (i % 2 == 0) {
+                result += "Codility ";
+            }else if (i % 3 == 0) {
+                result += "Test ";
+            }else if (i % 5 == 0) {
+                result += "Coders ";
+        }else{
+                result += i +" ";
+            }
+        }
+        System.out.println(result);
+
+        }
 
 
 
     }
-}
+
