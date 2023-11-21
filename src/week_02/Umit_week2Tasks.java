40178b4355142dbabf9b14a57377baa0bdb531ee
umittsr <umittsr@gmail.com>
2023-11-21 13:29:28 +0100

    Ümit's solutions for the tasks week 2

---
+++ src/week_02/Umit_week2Tasks.java
@@ -0,0 +1,24 @@
+package week_02;
+
+public class Umit_week2Tasks {
+
+    public static void main(String[] args) {
+
+        //1- swap two numbers--swap two variables' values without using a third variable
+
+        int a = 20; // 10
+        int b = 10; // 20
+
+        a = a + b;  // a = 20+10 = 30
+        b = a - b; // b = 30-10 = 20
+        a = a - b; // a = 30-20 = 10
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+
+    }
+}
