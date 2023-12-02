1f3cb95eda2ee97dee6e5e72edc2995e2ea66571
zlatan <zlatanbasyc@gmail.com>
2023-12-02 18:56:32 +0100

    Prime number task

--- src/week_03/Task1.java
+++ src/week_03/Task1.java
@@ -1,6 +1,8 @@
 package week_03;
 
 public class Task1 {
+
+
     //Prime number can be divided only with nr. 1 and itself
     static boolean isPrime(int n) {
         // Corner case
@@ -13,7 +15,27 @@ public class Task1 {
                 return false;
 
         return true;
+
     }
+
+    public static int reverseInteger(int number) {
+        boolean isNegative = number < 0 ? true : false;
+        if(isNegative){
+            number = number * -1;
+        }
+        int reverse = 0;
+        int lastDigit = 0;
+
+        while (number >= 1) {
+            lastDigit = number % 10; // gives you last digit
+            reverse = reverse * 10 + lastDigit;
+            number = number / 10; // get rid of last digit
+        }
+
+        return isNegative == true? reverse*-1 : reverse;
+    }
+
+
 
 
 
