380e83301f211c91ad17ff454e6c565457fcdd1d
Sayfulloh2407 <abdurazzoqzodasayfullo@gmail.com>
2023-12-18 10:22:33 +0100

    week5.Task solutions by Sayfulloh

---
+++ src/week_05/Sayfulloh.java
@@ -0,0 +1,30 @@
+package week_05;
+
+public class Sayfulloh {
+    public static void main(String[] arg){
+        System.out.println("unique(\"ABGGHFFACRCTR\") = " + unique("ABGGHFFACRCTR"));
+        System.out.println("Reverse(\"ABCD\") = " + Reverse("ABCD"));
+    }
+
+    public static String unique(String input){
+        String result="";
+        for (int i = 0; i < input.length(); i++) {
+            if (input.indexOf(input.charAt(i))==input.lastIndexOf(input.charAt(i))){
+                result+=input.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public static String Reverse(String input){
+        String result="";
+        for (int i=input.length()-1;i>=0;i--){
+            result+=input.charAt(i);
+        }
+        return result;
+    }
+
+
+
+
+}
