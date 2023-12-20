a73d086883d44f5fb6868bcd90244ab3aa3169e6
Ali Camoglu <119692910+alicamoglu@users.noreply.github.com>
2023-12-20 19:14:15 +0100

    updating to week5 Ali

--- src/week_05/Ali.java
+++ src/week_05/Ali.java
@@ -1,4 +1,4 @@
-package javaTaksMentor;
+package week_05;
 
 public class Ali {
 
@@ -6,17 +6,17 @@ public class Ali {
         String strForUnique = "AAABBBCCCDEF";
         String strForReverse = "ABCD";
 
-        System.out.println("unique " + strForUnique+ " ==> " + uniqueChars(strForUnique));
-        System.out.println("Reverse " + strForReverse +" ==> " +reverse(strForReverse));
+        System.out.println("unique " + strForUnique + " ==> " + uniqueChars(strForUnique));
+        System.out.println("Reverse " + strForReverse + " ==> " + reverse(strForReverse));
     }
 
-    public static String uniqueChars(String strA){  // removes repetition from a string and gives string
+    public static String uniqueChars(String strA) {  // removes repetition from a string and gives string
         String strB = "";
-        int aa = strA.length(), b=0;
+        int aa = strA.length(), b = 0;
 
         while (aa > b) {
 //            if (strA.substring(0, b).contains("" + strA.charAt(b))) {
-            if (countOccurrences(strA,(strA.charAt(b))) !=1) {  // if occurs a char more than one than it will not be added to result string
+            if (countOccurrences(strA, (strA.charAt(b))) != 1) {  // if occurs a char more than one than it will not be added to result string
 
             } else {
                 strB += strA.charAt(b);
@@ -25,6 +25,7 @@ public class Ali {
         }
         return strB;
     }
+
     static int countOccurrences(String str, char targetChar) {  // bir karakterin tekrar sayısını verir.
         int count = 0;
 
@@ -39,12 +40,29 @@ public class Ali {
     public static String reverse(String str) {
         String reverse = "";   // to contain all the characters of the given in reverse method;
 
-        for (int i = str.length()-1; i >=0 ; i--) {   // i : index number of the given string starting from last index to index 0
+        for (int i = str.length() - 1; i >= 0; i--) {   // i : index number of the given string starting from last index to index 0
             reverse += str.charAt(i);              //so that from the given string all characters will be able to added from last to first
 
         }
         return reverse;
     }
+
+    public static String uniqueChars2(String strA) {  // removes repetition from a string and gives string
+        String strB = "";
+//        int aa = strA.length(), b=0;
+
+        while (strA.length() > 0) {
+
+//            if (!countOccurrences(strA,(strA.charAt(0))==1  ) !=1) {
+            if (!strB.contains(strA.substring(0, 1)) && countOccurrences(strA, (strA.charAt(0))) == 1) {
+                strB += strA.charAt(0);
+                strA = strA.replaceAll(strA.substring(0, 1), "");
+            }
+
+        }
+        return strB;
+    }
+
 }
     /*      String -- Find the unique
             Write a return method that can find the unique characters from the String
