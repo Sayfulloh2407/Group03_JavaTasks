e8ac3fdf15bce6d4f54d6f666055459e3bf48803
umittsr <umittsr@gmail.com>
2024-01-13 22:19:34 +0100

    Ümit's solutions for the tasks week 8

--- src/week_08/ümit.java
+++ src/week_08/ümit.java
@@ -62,7 +62,12 @@ public class ümit {
     }
 
 
-
+   /*
+   Array - Move Zeros to the End
+Write a method that can move all the zeros to last indexes of the array (Do
+Not Use Sort Method) Ex: input: {1,0,2,0,3,0,4,0}; output: [1, 2, 3, 4, 0, 0, 0,
+0]
+    */
     public static int[] moveZeroToEnd(int[]array){
         int[] result = new int[array.length];
 
